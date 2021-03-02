88418f12153a01b98ab74fb845d9bd8854af4d6d
col1n <col1n@boston.local>
2021-03-02 17:22:55 +0100

    fix little bug in index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import Collection from "./components/Collection/Collection";
 import PartWhole from "./components/PartWhole/PartWhole";
 import Depiction from "./components/Resource/Depiction";
-import KG from "./components/KnowledgeGraph/KG";
-import Graph from "./components/classes/Graph";
 import { scaleData } from "./utilities/math";
 import findSliderDomain from "./components/filters/facets/findSliderDomain";
 import ImageGrid from "./components/ImageGrid/ImageGrid";
 import PropertyValueList from "./components/PropertyValueList/PropertyValueList";
 import Measurement from "./measurements/Measurement";
-import ODPReactor from "./components/KnowledgeGraph/ODPReactor";
 import KnowledgeGraph from "./classes/KnowledgeGraph";
 import Resource from "./classes/Resource";
 import ResourceFactory from "./classes/ResourceFactory";
@@ -20,9 +17,7 @@ import ResourcesPage from "./pages/ResourcesPage";
  * Available components
  */
 export {
-    ODPReactor,
     KnowledgeGraph,
-    Graph,
     Measurement,
     Resource,
     scaleData,
@@ -30,7 +25,6 @@ export {
     Collection,
     PartWhole,
     Depiction,
-    KG,
     ImageGrid,
     PropertyValueList,
     ResourceFactory,
