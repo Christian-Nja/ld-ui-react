df24067862e0ad34ec2a2da7c93a7f90ed92ad1c
col1n <col1n@boston.local>
2021-03-19 15:13:34 +0100

    modification on package.json for build command

--- src/components/filters/facets/MaxMeasurementSliderFilter.jsx
+++ src/components/filters/facets/MaxMeasurementSliderFilter.jsx
@@ -21,6 +21,8 @@ export default function MaxMeasurementSliderFilter({
     measurementType,
 }) {
     const formatTicks = (d) => {
+        console.log("FORMAT TICKS");
+        console.log(formatTicks);
         return Qty(`${d} ${measurementUnit}`).format("m");
     };
 
